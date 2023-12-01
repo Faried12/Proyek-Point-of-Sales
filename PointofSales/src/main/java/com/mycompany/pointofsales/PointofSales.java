785d6a35cf313ec2358fec6a9e16882a0534dcf9
Faried12 <fariedsputra@gqmil.com>
2023-12-01 08:19:04 +0700

    opsional

---
+++ PointofSales/src/main/java/com/mycompany/pointofsales/PointofSales.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pointofsales;
+
+/**
+ *
+ * @author HP
+ */
+public class PointofSales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("AKU ANAK PINTAR");
+    }
+}
