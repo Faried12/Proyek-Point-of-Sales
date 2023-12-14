b4bef3d08a9cc7d37a6e695c9c715f4b8dbf4435
Faried12 <fariedsputra@gmail.com>
2023-12-14 21:17:07 +0700

    New update

--- PointofSales/src/main/java/com/mycompany/pointofsales/FirstLogin.java
+++ PointofSales/src/main/java/com/mycompany/pointofsales/FirstLogin.java
@@ -20,36 +20,5 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class FirstLogin {
-    // Informasi koneksi database
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/point_of_sales";
-    private static final String DB_USER = "username_database";
-    private static final String DB_PASSWORD = "password_database";
-
-    public static void main(String[] args) {
-    
-    }
-    // Fungsi untuk memvalidasi login dari database
-    private static boolean validateLogin(String username, String password) {
-        try {
-            // Buat koneksi
-            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-
-            // Query untuk memeriksa username dan password
-            String query = "SELECT * FROM users WHERE username = ? AND password = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setString(1, username);
-                preparedStatement.setString(2, password);
-
-                // Eksekusi query
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-                // Jika hasil query tidak kosong, berarti login berhasil
-                return resultSet.next();
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            return false;
-        }
-    }
 }
 
